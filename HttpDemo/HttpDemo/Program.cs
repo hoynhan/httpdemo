ea3b7e671047dddc0100ef61e9121e5a84b5c5e9
Thong Nguyen <thong.nguyenminhthonghcm@outlook.com>
2025-03-01 00:41:42 +0700

    add intergration test

--- HttpDemo/HttpDemo/Program.cs
+++ HttpDemo/HttpDemo/Program.cs
@@ -1,44 +1,40 @@
 using HttpDemo.SessionStorage;
+using System.IO;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-
-builder.Services.AddSingleton<IMySessionStorageEngine>(service =>
+namespace HttpDemo
 {
-    var path = Path.Combine(service.GetRequiredService<IHostEnvironment>().ContentRootPath, "sessions");
-    Directory.CreateDirectory(path);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-    return new FileSessionStorageEngine(path);
-});
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddMySession();
 
-builder.Services.AddSingleton<IMySessionStorage, MySessionStorage>();   
-builder.Services.AddScoped<MySessionScopedContainer>();
+            var app = builder.Build();
 
-var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
-app.Run();
-
---
+++ HttpDemo/MySessionIntergrationTest/SessionIntergrationTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace MySessionIntergrationTest
+{
+    public class SessionIntergrationTest: IClassFixture<WebApplicationFactory<HttpDemo.Program>>
+    {
+        private readonly HttpClient _factory;
+
+        public SessionIntergrationTest(WebApplicationFactory<HttpDemo.Program> factory)
+        {
+            this._factory = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Call_TestGetSession_Return_Ok_Async()
+        {
+            var response = await _factory.GetAsync("/Test/TestGetSession");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Call_Set_And_GetSessionValueAsync_Return_Ok_Async()
+        {
+            string randomValue = Guid.NewGuid().ToString();
+            await _factory.GetAsync($"/Test/SetSessionValue?key=TEST-Key&value={randomValue}");
+
+            var response = await _factory.GetAsync("/Test/GetSessionValue?key=TEST-Key");
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(randomValue, responseString);
+        }
+    }
+}
